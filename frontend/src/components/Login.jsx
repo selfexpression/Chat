9523d4e0937da9aff0be821e1514b970154abcb1
Artem <selfex9496@gmail.com>
2023-08-31 17:03:01 +0700

    impl authorization

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik';
+import { useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
 import axios from 'axios';
 import image from '../assets/login.jpeg';
@@ -12,43 +15,68 @@ const schema = Yup.object().shape({
     .required(),
 });
 
-const LoginForm = () => (
-  <Formik
-    initialValues={{ username: '', password: '' }}
-    validationSchema={schema}
-    onSubmit={async (values) => {
-      const response = axios.post(routes.loginPath(), values);
-      console.log(response);
-    }}
-  >
-    {() => (
-      <Form className="col-12 col-md-6 mt-3 mt-mb-0">
-        <h1 className="text-center mb-4">Войти</h1>
-        <div className="form-floating mb-3">
-          <Field
-            type="text"
-            name="username"
-            id="username"
-            className="form-control"
-            placeholder="Ваш ник"
-          />
-          <label htmlFor="username" className="form-label">Ваш ник</label>
-        </div>
-        <div className="form-floating mb-4">
-          <Field
-            type="password"
-            name="password"
-            id="password"
-            className="form-control"
-            placeholder="Пароль"
-          />
-          <label htmlFor="password" className="form-label">Пароль</label>
-        </div>
-        <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
-      </Form>
-    )}
-  </Formik>
-);
+const LoginForm = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      validationSchema={schema}
+      onSubmit={async (values, { setSubmitting, setErrors }) => {
+        try {
+          const response = await axios.post(routes.loginPath(), values);
+          const { token } = response.data;
+          localStorage.setItem('userId', token);
+
+          const userId = localStorage.getItem('userId');
+
+          if (!userId) {
+            navigate('/login');
+            return;
+          }
+
+          navigate('/');
+          setSubmitting(false);
+        } catch (error) {
+          setErrors({ auth: error.message });
+        }
+      }}
+    >
+      {({ errors }) => (
+        <Form className="col-12 col-md-6 mt-3 mt-mb-0">
+          <h1 className="text-center mb-4">Войти</h1>
+          <div className="form-floating mb-3">
+            <Field
+              type="text"
+              name="username"
+              id="username"
+              className={`form-control ${errors.auth ? 'is-invalid' : ''}`}
+              placeholder="Ваш ник"
+            />
+            <ErrorMessage
+              component="div"
+              name="username"
+              className="invalid-feedback"
+            />
+            <label htmlFor="username" className="form-label">Ваш ник</label>
+          </div>
+          <div className="form-floating mb-4">
+            <Field
+              type="password"
+              name="password"
+              id="password"
+              className={`form-control ${errors.auth ? 'is-invalid' : ''}`}
+              placeholder="Пароль"
+            />
+            {errors.auth ? (<div className="invalid-tooltip">{errors.auth}</div>) : null}
+            <label htmlFor="password" className="form-label">Пароль</label>
+          </div>
+          <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 const Footer = () => (
   <div className="card-footer p-4">
