250213d76ee90782b6405da46db17640cac2240c
Artem <selfex9496@gmail.com>
2023-09-01 19:02:04 +0700

    rename, socket.io install

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginForm from './Login.jsx';
 import NotFound from './NotFound.jsx';
-import Chat from './Chat.jsx';
+import Channels from './Channels.jsx';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Chat />} />
+      <Route path="/" element={<Channels />} />
       <Route path="/login" element={<LoginForm />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
