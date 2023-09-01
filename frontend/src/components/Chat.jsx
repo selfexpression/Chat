1296235e1a16b423589e00ae0feec3d1a2830121
Artem <selfex9496@gmail.com>
2023-09-01 17:55:35 +0700

    impl axios data channels, render channels

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,16 +1,60 @@
+import axios from 'axios';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectors, getAxiosData } from '../slices/dataSlice.js';
+import { actions as dataActions, selectors } from '../slices/dataSlice.js';
+import routes from '../routes.js';
+
+const getAuthHeader = () => {
+  const userId = localStorage.getItem('userId');
+
+  if (userId) {
+    return { Authorization: `Bearer ${userId}` };
+  }
+
+  return {};
+};
 
 const Chat = () => {
   const dispatch = useDispatch();
-  const data = useSelector(selectors.selectAll);
 
   useEffect(() => {
-    dispatch(getAxiosData());
+    const getAxiosData = async () => {
+      const headers = getAuthHeader();
+      const response = await axios.get(routes.dataPath(), { headers });
+      dispatch(dataActions.addData(response.data));
+    };
+
+    getAxiosData();
   });
 
-  console.log(data);
+  const data = useSelector(selectors.selectAll);
+  const channels = data.flatMap((entity) => entity.channels);
+
+  return (
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+        <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+          <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+            <b>Каналы</b>
+            <button type="button" className="p-0 text-primary btn btn-group-vertical">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={20} height={20} fill="currentColor" />
+              <span className="visually-hidden">+</span>
+            </button>
+          </div>
+          <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
+            {channels.map(({ id, name }) => (
+              <li key={id} className="nav-item w-100">
+                <button type="button" className="w-100 rounded-0 text-start btn">
+                  <span className="me-1">#</span>
+                  {name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- frontend/src/slices/dataSlice.js
+++ frontend/src/slices/dataSlice.js
@@ -1,35 +1,18 @@
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
-import axios from 'axios';
-import routes from '../routes.js';
 
-const getAuthHeader = () => {
-  const userId = localStorage.getItem('userId');
-
-  if (userId) {
-    return { Authorization: `Bearer ${userId}` };
-  }
-
-  return {};
-};
-
-export const getAxiosData = async () => {
-  const headers = getAuthHeader();
-  const response = await axios.get(routes.dataPath(), { headers });
-  return response.data;
-};
-
-const dataAdapter = createEntityAdapter();
+const dataAdapter = createEntityAdapter({
+  selectId: (channel) => channel.currentChannelId,
+});
 const initialState = dataAdapter.getInitialState();
 
 const slice = createSlice({
   name: 'data',
   initialState,
-  extraReducers: (builder) => {
-    builder
-      .addCase(getAxiosData.fulfilled, dataAdapter.addMany);
+  reducers: {
+    addData: dataAdapter.addOne,
   },
 });
 
 export const selectors = dataAdapter.getSelectors((state) => state.data);
-
+export const { actions } = slice;
 export default slice.reducer;
